08592e4b60f1d6ba43b50a43346f66b8783dbace
ThomasHuynh <Thomas_TTH@live.com>
2023-11-17 15:00:45 -0500

    Installed required packages, styled nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="flex justify-between items-center p-4 absolute w-full z-100">
+      <h1 className="text-red-600 uppercase text-4xl font-bold cursor-pointer">Netflix</h1>
+      <div>
+        <button className="text-white mr-4 hover:text-white/90">Sign In</button>
+        <button className="bg-red-600 text-white hover:bg-red-700 px-6 py-4 rounded cursor-pointer">Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
