6974858b78d0402bfda98383bb7e985be97bdca5
ThomasHuynh <Thomas_TTH@live.com>
2023-11-23 23:02:12 -0500

    Added subtle text and backdrop animation on load

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -43,16 +43,16 @@ const Main = () => {
 
   return (
     <div className="featuredImg w-full relative text-white h-[550px] xl:h-[700px]">
-      <div className="absolute w-full h-[550px] xl:h-[700px] bg-gradient-to-r from-black"></div>
+      <div className="absolute w-full h-[550px] xl:h-[700px] bg-gradient-to-r from-black z-10"></div>
       {featuredMovie?.backdrop_path && (
         <img
           src={`https://image.tmdb.org/t/p/original/${featuredMovie?.backdrop_path}`}
           alt={featuredMovie?.title}
-          className="w-full h-full object-cover object-top transition"
+          className="w-full h-full object-cover object-top fadeIn"
         />
       )}
-      <div className="absolute top-[50%] translate-y-[-50%] p-6 max-w-[90%] md:max-w-[65%] xl:max-w-[40%]">
-        <div>
+      <div className="absolute top-[50%] translate-y-[-50%] p-6 max-w-[90%] md:max-w-[65%] xl:max-w-[40%] z-20">
+        <div className="fadeSmooth">
           <h1 className="text-3xl sm:text-5xl my-4">{featuredMovie?.title}</h1>
           <p className="text-sm sm:text-base">
             {truncateStr(featuredMovie?.overview, 200)}
@@ -61,15 +61,15 @@ const Main = () => {
             <FaStar className="text-yellow-300 mr-2" />
             {featuredMovie?.vote_average?.toFixed(1)}
           </p>
-        </div>
-        <div>
-          <button
-            onClick={() => handleMovieData(featuredMovie.id)}
-            className="bg-red-600 text-white border border-red-600 hover:bg-red-700 hover:border-red-700 flex items-center px-6 py-3 rounded cursor-pointer mr-4 text-sm sm:text-base"
-          >
-            <FaPlay className="mr-2"/>
-            Play
-          </button>
+          <div>
+            <button
+              onClick={() => handleMovieData(featuredMovie.id)}
+              className="bg-red-600 text-white border border-red-600 hover:bg-red-700 hover:border-red-700 flex items-center px-6 py-3 rounded cursor-pointer mr-4 text-sm sm:text-base"
+            >
+              <FaPlay className="mr-2" />
+              Play
+            </button>
+          </div>
         </div>
       </div>
     </div>
